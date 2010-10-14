37e68bc259b448191edd641d03a56baeda349d6b
David Lynch <kemayo@gmail.com>
2010-10-14 16:00:26 -0500

    Rearranging of buff frames for 4.0.1

--- addon.lua
+++ addon.lua
@@ -2,3 +2,5 @@
 ConsolidatedBuffs:ClearAllPoints()
 ConsolidatedBuffs:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -190, -45)
 ConsolidatedBuffs.SetPoint = function() end
+BuffFrame:ClearAllPoints()
+BuffFrame:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -190, -45)
