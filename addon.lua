c6a5775247b46a5d3fd546127a76db434e45a7f8
David Lynch <kemayo@gmail.com>
2026-01-25 15:45:20 -0600

    Commented out checkbox and dressupmodel testing

--- addon.lua
+++ addon.lua
@@ -23,3 +23,57 @@ StackCloth = BankStack.CommandDecorator(function(from, to)
     BankStack.Stack(from, to, is_cloth)
     BankStack.Fill(from, to, false, is_cloth)
 end, "bags bank", 2)
+
+-- Horrifying checkboxes
+
+-- local options = {
+--   type = "group",
+--   name = "Testing of toggle",
+--   get = function(info) return true end,
+--   set = function(info, value) end,
+--   args = {},
+-- }
+-- for i=1,1000 do
+--   options.args["toggle" .. i] = {
+--     type = "toggle",
+--     name = "Toggle #" .. i,
+--     width = "full",
+--   }
+-- end
+
+-- LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("ToggleTest", options)
+-- LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ToggleTest", "ToggleTest")
+
+-- DressUpModel customrace bug:
+
+-- local model = CreateFrame("DressUpModel", nil, UIParent)
+-- model:SetSize(300, 500)
+-- model:SetPoint("CENTER")
+-- model:SetKeepModelOnHide(true)
+-- model:Show()
+
+-- -- model:SetBarberShopAlternateForm()
+-- -- model:SetUnit("player")
+-- model:SetUnit("none")
+-- model:SetCustomRace(1, 1) -- human female
+-- model:TryOn("item:128365") -- a distinctive hat
+
+
+-- local model = CreateFrame("DressUpModel", "MAWDSModel", UIParent)
+-- model:SetSize(300, 500)
+-- model:SetPoint("CENTER")
+-- model:SetKeepModelOnHide(true)
+-- model:Show()
+
+-- -- model:SetBarberShopAlternateForm()
+-- model:SetUnit("player")
+-- -- model:SetUnit("none")
+-- model:SetScript("OnUpdate", function()
+--   if IsUnitModelReadyForUI("player") then
+--     model:RefreshUnit()
+--     -- model:Undress()
+--     model:UndressSlot()
+--     model:TryOn("item:128365") -- a distinctive hat
+--     model:SetScript("OnUpdate", nil)
+--   end
+-- end)
